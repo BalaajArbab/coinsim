fa266bed0ea46a045b659bb25a10342f2ac78527
Anubhav Aery <aaery3@uwo.ca>
2022-03-31 15:19:56 -0400

    Created authentication package with two java files: authentication.java and login.java

---
+++ src/main/java/coinSim/authentication/authentication.java
@@ -0,0 +1,5 @@
+package coinSim.authentication;
+
+public class authentication {
+
+}
---
+++ src/main/java/coinSim/authentication/login.java
@@ -0,0 +1,10 @@
+package coinSim.authentication;
+
+public class login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
