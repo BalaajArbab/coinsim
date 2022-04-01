129ee0bf2ade9c7aaa6173cccdedcc822e1089d7
1RishabhJain <rishabhjain0501@gmail.com>
2022-04-01 19:26:42 -0400

    program passes test credentials to authentication function and returns validity

--- src/main/java/coinSim/gui/LoginUI.java
+++ src/main/java/coinSim/gui/LoginUI.java
@@ -1,13 +1,20 @@
 package coinSim.gui;
 
+
 import coinSim.authentication.authentication;
 
 public class LoginUI extends authentication {
 	public boolean userLogin() {
 	    System.out.println("Logging In");
+	    
 	    authentication auth = new authentication();
+	    
+	    
+	    String user = "rishabh";
+	    String pass = "password1";
+	    String creds = user + "," + pass;
 	    // calling validCredentials method of authentication
-	    boolean validUser = auth.validCredentials();
+	    boolean validUser = auth.validCredentials(creds);
 	    return validUser;
 	  }
 }
