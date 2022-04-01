0988261410e811ab3249243349d2b88d0dd32703
1RishabhJain <rishabhjain0501@gmail.com>
2022-04-01 18:01:22 -0400

    proof of concept for user auth

--- src/main/java/coinSim/authentication/authentication.java
+++ src/main/java/coinSim/authentication/authentication.java
@@ -1,5 +1,9 @@
 package coinSim.authentication;
 
 public class authentication {
-
+	// Account valid
+	public boolean validCredentials() {
+		System.out.println("return true");
+		return true;
+	}
 }
--- src/main/java/coinSim/gui/LoginUI.java
+++ src/main/java/coinSim/gui/LoginUI.java
@@ -1,5 +1,13 @@
 package coinSim.gui;
 
-public class LoginUI {
+import coinSim.authentication.authentication;
 
+public class LoginUI extends authentication {
+	public boolean userLogin() {
+	    System.out.println("Logging In");
+	    authentication auth = new authentication();
+	    // calling validCredentials method of authentication
+	    boolean validUser = auth.validCredentials();
+	    return validUser;
+	  }
 }
