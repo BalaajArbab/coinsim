e015582b8c0fbb0cf8d30e5ff3313b0a73dbb870
Anubhav Aery <aaery3@uwo.ca>
2022-04-02 14:23:54 -0400

    Implemented UI page to select from either login or create and then advance to the specified page

--- src/main/java/coinSim/gui/Runner.java
+++ src/main/java/coinSim/gui/Runner.java
@@ -1,15 +1,102 @@
 package coinSim.gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import coinSim.authentication.authentication;
 import coinSim.authentication.login;
 
-public class Runner extends login{
+public class Runner implements ActionListener{
+	private static JLabel userLabel;
+	private static JTextField userText;
+	private static JLabel passwordLabel;
+	private static JPasswordField passwordText;
+	private static JButton button;
+	private static JButton button2;
+	private static JLabel success;
+	private static boolean finalResult;
+	private static String user;
+	private static String pass;
+	private static JFrame frame;
+	private static String [] Args;	
+	
+	
+
 	public static void main(String[] args) {
-	    // creating object of LoginUI
-		//LoginUI lgn = new LoginUI();
-	    // calling userLogin method of LoginUI
-	    //boolean result = lgn.userLogin();
 		
-		LoginUI.main(args);
-	    //System.out.print(result);
-	  }
+		Args=args;
+		//creating frame
+		frame = new JFrame();
+		frame.setSize(350,200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//creating panel
+		JPanel panel = new JPanel();
+		//Put Panel on the frame
+		frame.add(panel);
+		
+		//Configuring the panel
+		panel.setLayout(null);
+		
+		
+		//Adding Login Button
+		button = new JButton("Login");
+		button.setBounds(10,80,150,25);
+		
+		//Adding button action
+		button.addActionListener(new Runner());
+		panel.add(button);
+		
+		//Adding Login Button
+		button2 = new JButton("Create Account");
+		button2.setBounds(180,80,150,25);
+		
+		//Adding button action
+		button2.addActionListener(new Runner());
+		panel.add(button2);
+		
+		
+		
+		frame.setVisible(true);
+		//////////////////////////////////////////
+		//LoginUI.main(args);
+		
+	  
+
+	button.addActionListener(new ActionListener() {
+
+        @Override
+        public void actionPerformed(ActionEvent event) {
+        	System.out.println("1 clicked");
+        	frame.dispose();
+        	LoginUI.main(Args);
+        }
+    });
+	
+	button2.addActionListener(new ActionListener() {
+
+        @Override
+        public void actionPerformed(ActionEvent event) {
+        	System.out.println("2 clicked");
+        	frame.dispose();
+        	CreateAccountUI.main(Args);
+        }
+    });
+
+	}
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
