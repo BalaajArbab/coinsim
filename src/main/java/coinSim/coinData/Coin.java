57516a5541e3708176156af579a3d29152a3a234
1RishabhJain <rishabhjain0501@gmail.com>
2022-04-04 18:25:04 -0400

    Formatting and comments

--- src/main/java/coinSim/coinData/Coin.java
+++ src/main/java/coinSim/coinData/Coin.java
@@ -6,49 +6,42 @@
 package coinSim.coinData;
 
 public class Coin {
-	
+
 	String Id;
 	String Symbol;
-	
+
 	double Price;
 	double MktCap;
 	double Volume;
-	
-	public Coin(String id, String symbol, double price, double mktCap, double volume)
-	{
+
+	public Coin(String id, String symbol, double price, double mktCap, double volume) {
 		this.Id = id;
 		this.Symbol = symbol;
 		this.Price = price;
 		this.MktCap = mktCap;
 		this.Volume = volume;
 	}
-	
-	public void UpdateCoin(double newPrice, double newMktCap, double newVolume)
-	{
+
+	public void UpdateCoin(double newPrice, double newMktCap, double newVolume) {
 		this.Price = newPrice;
 		this.MktCap = newMktCap;
 		this.Volume = newVolume;
 	}
-	
-	public String toString()
-	{
+
+	public String toString() {
 		return "ID: " + this.Id + ", Price: " + this.Price + ", MktCap: " + this.MktCap + ", Volume: " + this.Volume;
 	}
-	
-	public double GetPrice()
-	{
+
+	public double GetPrice() {
 		return this.Price;
 	}
-	
-	public double GetMktCap()
-	{
+
+	public double GetMktCap() {
 		return this.MktCap;
 	}
-	
-	public double GetVolume()
-	{
+
+	public double GetVolume() {
 		return this.Volume;
 	}
-	
 
 }
